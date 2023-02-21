d433e92c34aad6ab45b78cddb6e8a48a6a9cad16
shellyscheng <shellysc13@gmail.com>
2023-02-20 16:59:56 -0800

    test: testing mocks

--- src/__tests__/useBreedList.test.jsx
+++ src/__tests__/useBreedList.test.jsx
@@ -1,19 +1,19 @@
 import { expect, test } from "vitest";
-import { renderHook } from "@testing-library/react";
+import { renderHook, waitFor } from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import useBreedList from "../useBreedList";
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staletTime: Infinity,
+      staleTime: Infinity,
       cacheTime: Infinity,
-      retry: false, // no retry in test, fail instead
+      retry: false, // no retry, fail the test instead
     },
   },
 });
 
-test("give an empty with no animal provided", async () => {
+test("gives an empty list with no animal", async () => {
   const { result } = renderHook(() => useBreedList(""), {
     wrapper: ({ children }) => (
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
@@ -24,4 +24,32 @@ test("give an empty with no animal provided", async () => {
 
   expect(breedList).toHaveLength(0);
   expect(status).toBe("loading");
+});
+
+test("gives back breeds with an animal", async () => {
+  const breeds = [
+    "Havanese",
+    "Bichon Frise",
+    "Poodle",
+    "Maltese",
+    "Golden Retriever",
+    "Labrador",
+    "Husky",
+  ];
+  fetch.mockResponseOnce(
+    JSON.stringify({
+      animal: "dog",
+      breeds,
+    })
+  );
+  const { result } = renderHook(() => useBreedList("dog"), {
+    wrapper: ({ children }) => (
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    ),
+  });
+
+  await waitFor(() => expect(result.current[1]).toBe("success"));
+
+  const [breedList] = result.current;
+  expect(breedList).toEqual(breeds);
 });
