e3c66e181e1cc522f3f753986632bfea8894d95b
shellyscheng <shellysc13@gmail.com>
2023-02-22 17:24:29 -0800

    feat: added tailwind css plugin forms

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -22,8 +22,10 @@ const SearchParams = () => {
   const pets = results?.data?.pets ?? [];
 
   return (
-    <div className="search-params">
+    <div className="my-0 mx-auto w-11/12">
       <form
+        className="mb-10 flex flex-col items-center justify-center rounded-lg bg-gray-200 p-10 shadow-lg
+      "
         onSubmit={(e) => {
           e.preventDefault();
           const formData = new FormData(e.target); // browser API
@@ -42,13 +44,20 @@ const SearchParams = () => {
         ) : null}
         <label htmlFor="location">
           Location
-          <input name="location" id="location" placeholder="location" />
+          <input
+            type="text"
+            name="location"
+            id="location"
+            className="mb-5 block w-60"
+            placeholder="location"
+          />
         </label>
         <label htmlFor="animal">
           Animal
           <select
             id="animal"
             name="animal"
+            className="mb-5 block w-60"
             onChange={(e) => {
               setAnimal(e.target.value);
             }}
@@ -61,14 +70,21 @@ const SearchParams = () => {
         </label>
         <label htmlFor="breed">
           Breed
-          <select id="breed" disabled={breeds.length === 0} name="breed">
+          <select
+            id="breed"
+            className="mb-5 block w-60 disabled:opacity-50"
+            disabled={breeds.length === 0}
+            name="breed"
+          >
             <option />
             {breeds.map((breed) => (
               <option key={breed}>{breed}</option>
             ))}
           </select>
         </label>
-        <button>Submit</button>
+        <button className="color rounded border-none bg-orange-500 px-6 py-2 text-white hover:opacity-50">
+          Submit
+        </button>
       </form>
       <Results pets={pets} />
     </div>
